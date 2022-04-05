73218734d440f446e042c615054e73409b177049
anh nguyen <nguyenlamanh1701@gmail.com>
2022-04-05 23:59:39 +0700

    generic list and detail views

--- catalog/views.py
+++ catalog/views.py
@@ -1,8 +1,24 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from django.views import generic
 from catalog.models import Author, Book, BookInstance
 
 # Create your views here.
+
+
+class BookListView(generic.ListView):
+    model = Book
+    paginate_by = 2
+    context_object_name = 'book_list'
+    queryset = Book.objects.filter()[:5]
+    template_name = 'books/my_arbitrary_template_name_list.html'
+
+
+class BookDetailView(generic.DetailView):
+    model = Book
+
+    def book_detail_view(request, primary_key):
+        book = get_object_or_404(Book, pk=primary_key)
+        return render(request, 'catalog/book_detail.html', context={'book': book})
 
 
 def index(request):
@@ -13,11 +29,15 @@ def index(request):
 
     num_authors = Author.objects.count()
 
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
+
     context = {
         'num_books': num_books,
         'num_instances': num_instances,
         'num_instances_available': num_instances_available,
         'num_authors': num_authors,
+        'num_visits': num_visits,
     }
 
     return render(request, 'index.html', context=context)
